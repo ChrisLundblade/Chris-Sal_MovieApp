18bd7f6b825d7f7569f638e7b57a21dcf1552cb6
Christopher Lundblade <chris.lundblade@gmail.com>
2021-08-31 14:21:28 -0500

    wrote initial functions that add edit and delete

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,62 +1,124 @@
 $(document).ready( function(){
     "use strict";
 
-    const url = "https://https://handsome-zany-staircase.glitch.me/movies"
-    // const url = "https://ruddy-imaginary-peony.glitch.me/movies";
+    const moviesURL = "https://handsome-zany-staircase.glitch.me/movies";
+    //const moviesURL = "https://ruddy-imaginary-peony.glitch.me/movies";
     const mainRow = $('.main-row')//in html file
-    let modalContainer = $('.modal-container')//in html and at bottom
-    let mainHTML = ""
-    let modelHTML = ""                        //at bottom creatmodel function
+    //in html and at bottom
+    //at bottom creatmodel function
 
 
 
     //!!!!need this to fetch data , all info from json move file on glitch
     const fetchData = () => {
-
         mainRow.toggle('hidden')
 
-        fetch(url)
+        fetch(moviesURL)
             .then(res => res.json())
             .then(data => {
-                renderHTML(data);
+                $('#loading').toggle('hidden')
+                return renderHTML(data);
             })
-
             .then(mainRow.toggle('hidden'))
             .catch(error => console.error(error))
     }
 
+    const deleteMovie = (id) =>{
+        let options = {
+            method: "DELETE",
+            headers: {
+                'Content-Type' : 'application/json',
+            }
+        };
+        $('#loading').toggle('hidden');
+        fetch(`${moviesURL}/${id}`, options)
+
+        // const asyncFoo = async () => {
+        //     await new Promise((resolve) => setTimeout(resolve, 2000));
+        //     console.log('nice');
+        //     console.log('way better');
+        //     fetchData()
+        // }
+    }
+
+    const editMovie = (id, newBodyObject) =>{
+        let options = {
+            method: "PUT",
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({
+                title: "The Room",
+                rating: 0,
+                id: id
+            })
+        };
+        fetch(`${moviesURL}/${id}`, options)
+            //.then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
+    const addMovie = () =>{
+        let options = {
+            method: "POST",
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({
+                title: "Shaun of the Dead",
+                rating: 4,
+                id: 8
+            })
+        };
+        $('#loading').toggle('hidden');
+        fetch(moviesURL, options);
+        fetchData();
+
+    }
 
     //!!!!!renders onto cards
     const renderHTML = data => {
-        mainHTML = ""
-        modelHTML = ""
-        createMovie(data)
+        let mainHTML = ""
+        let modelHTML = ""
+        createMovie(modelHTML)
         for(let obj of data) {
-            mainHTML += `<div >
+            mainHTML += `<div>
                                 <div class="card" style="width: 18rem;">
                                        <div class="info${obj.id} "><!-- sset to hidden to hide cards along with const loading = $('.loading') at top-->
                                             <div class="card-body">
-                                                <h5 >${obj.title}</h5>
-                                                <p >Rating: ${obj.rating}</p>
+                                                <h5>${obj.title}</h5>
+                                                <p>Rating: ${obj.rating}</p>
+                                                <button class="edit">Edit</button>
+                                                <button class="delete" id="${obj.id}">Delete</button>
                                             </div>
                                       </div>
                                 </div>
                           </div>`
-
-
         }
         mainRow.html(mainHTML)
 
     }
+
+    //Listeners for buttons
+    $(document).click(function(e){
+        if($(e.target).hasClass('delete')){
+            deleteMovie(e.target.id);
+        }
+    });
+
     //card popup
     const createMovie = data => {
 
-        modalContainer.html(modelHTML)
+        $('.model-container').html(data)
     }
 
 
 
     fetchData();
+    //setTimeout(addMovie(), 5000)
+    //setTimeout(deleteMovie(8), 15000);
+    //editMovie(2, {});
+
+    //setTimeout(addMovie(), 30000)
 
 })
 
